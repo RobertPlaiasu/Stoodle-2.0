867e0a0d6862f1fc7da712c779ecd9a0826a52ce
GrigoAleex <grigorescu.aleex@gmail.com>
2020-06-17 19:17:37 +0300

    Landing Page

--- Stoodle/resources/views/welcome.blade.php
+++ Stoodle/resources/views/welcome.blade.php
@@ -4,64 +4,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Stoodle</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -70,31 +19,47 @@
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ route('login') }}">Login</a>
+                        <a href="{{ route('login') }}">logare</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
+                            <a href="{{ route('register') }}">inregistrare</a>
                         @endif
                     @endauth
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+            <div class="text-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <h1 id="main-title"> 
+                                Dezvolta-te in viata si in cariera
+                            </h1>
+                            <p class="mb-5">
+                            Viața ta începe în momentul în care începi să iei decizii. Decizii 
+                        importante care îți marchează tot <br> viitorul. Și sigur nu vrei să fie unele 
+                        greșite. Pentru a fi sigur că te îndrepți spre reușită, ia cele mai <br> bune 
+                        decizii pentru tine, în funcție de aptitudinile pe care le ai.
+                            </p>
+                            <div class="links">
+                            <a href="{{ route('login') }}"> Incepe </a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+
+            <div class="cookie-container">
+                <p class="p-2">
+                    Folosim cookie-uri pe acest site web pentru a vă oferi cea mai bună experiență pe site-ul nostru și pentru a vă afișa reclame relevante. Pentru a afla mai multe, citiți
+                    <a href="#">politica noastră de confidențialitate</a> și <a href="#">olitica privind cookie-urile</a>.
+                </p>
+                <button class="cookie-btn">
+                Okay
+                </button>
+            </div>
+
         </div>
     </body>
 </html>
