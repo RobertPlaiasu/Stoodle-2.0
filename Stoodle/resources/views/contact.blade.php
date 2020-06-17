fac5ffdb605902fa81ea0d4bfc9a54304ee3f427
GrigoAleex <grigorescu.aleex@gmail.com>
2020-06-17 23:11:01 +0300

    contact page

--- Stoodle/resources/views/contact.blade.php
+++ Stoodle/resources/views/contact.blade.php
@@ -20,4 +20,20 @@
 
 @include('inc.navbar')
 
-CONTACT
+<div id="showcase">
+    <div class="content d-flex align-items-center justify-content-center">
+        <div class="college-card text-center">
+            <img src="./Images/Grigo.jpg" alt="Poza cu Grigo">
+            <h3>Grigorescu Alexandru</h3>
+            <h6>Front-End Developer</h6>
+            <p>Ma consider o persoana joviala</p>
+        </div>
+
+        <div class="college-card text-center">
+            <img src="./Images/Robert.jpg" alt="Poza cu Robert">
+            <h3>Plaiasu Robert</h3>
+            <h6>Back-End Developer</h6>
+            <p>Imi place sa ma uit la seriale</p>
+        </div>
+    </div>
+</div>
