b6f0c521c2bd000c24340ab6de08ded7cfc662c5
GrigoAleex <grigorescu.aleex@gmail.com>
2020-06-24 16:40:08 +0300

    Completed the county seeder

--- Stoodle/database/seeds/CountySeeder.php
+++ Stoodle/database/seeds/CountySeeder.php
@@ -13,10 +13,124 @@ class CountySeeder extends Seeder
     {
         DB::table('counties')->insert(array(
           array(
-            'county' => 'Prahova'
+            'county' => 'Alba'
+          ),
+          array(
+            'county' => 'Arad'
+          ),
+          array(
+            'county' => 'Arges'
+          ),
+          array(
+            'county' => 'Bacau'
+          ),
+          array(
+            'county' => 'Bihor'
+          ),
+          array(
+            'county' => 'Bistrita-Nasaud'
+          ),
+          array(
+            'county' => 'Botosani'
+          ),
+          array(
+            'county' => 'Braila'
+          ),
+          array(
+            'county' => 'Brasov'
+          ),
+          array(
+            'county' => 'Buzau'
+          ),
+          array(
+            'county' => 'Calarasi'
           ),
           array(
             'county' => 'Cluj'
+          ),
+          array(
+            'county' => 'Caras-Severin'
+          ),
+          array(
+            'county' => 'Constanta'
+          ),
+          array(
+            'county' => 'Covasna'
+          ),
+          array(
+            'county' => 'Dambovita'
+          ),
+          array(
+            'county' => 'Dolj'
+          ),
+          array(
+            'county' => 'Galati'
+          ),
+          array(
+            'county' => 'Giurgiu'
+          ),
+          array(
+            'county' => 'Harghita'
+          ),
+          array(
+            'county' => 'Hunedoara'
+          ),
+          array(
+            'county' => 'Ialomita'
+          ),
+          array(
+            'county' => 'Iasi'
+          ),
+          array(
+            'county' => 'Ilfov'
+          ),
+          array(
+            'county' => 'Maramures'
+          ),
+          array(
+            'county' => 'Mehedinti'
+          ),
+          array(
+            'county' => 'Mures'
+          ),
+          array(
+            'county' => 'Neamt'
+          ),
+          array(
+            'county' => 'Olt'
+          ),
+          array(
+            'county' => 'Prahova'
+          ),
+          array(
+            'county' => 'Salaj'
+          ),
+          array(
+            'county' => 'Satu-Mare'
+          ),
+          array(
+            'county' => 'Sibiu'
+          ),
+          array(
+            'county' => 'Suceava'
+          ),
+          array(
+            'county' => 'Teleorman'
+          ),
+          array(
+            'county' => 'Timis'
+          ),
+          array(
+            'county' => 'Tulcea'
+          ),
+          array(
+            'county' => 'Valcea'
+          ),
+          array(
+            'county' => 'Vaslui'
+          ),
+          array(
+            'county' => 'Vrancea'
           ),
         ));
     }
