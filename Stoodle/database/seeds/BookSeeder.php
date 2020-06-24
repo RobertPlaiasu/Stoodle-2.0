93aa2baeef37e99fd2fe867b4488bee16ad6d5a3
GrigoAleex <grigorescu.aleex@gmail.com>
2020-06-24 16:36:03 +0300

    Completed the book seeder

--- Stoodle/database/seeds/BookSeeder.php
+++ Stoodle/database/seeds/BookSeeder.php
@@ -13,10 +13,73 @@ class BookSeeder extends Seeder
     {
         DB::table('books')->insert(array(
           array(
+            'book' => 'Culinare'
+          ),
+          array(
+            'book' => 'Arte, tehnica'
+          ),
+          array(
+            'book' => 'Enciclopedii'
+          ),
+          array(
+            'book' => 'Biografii, memorii'
+          ),
+          array(
+            'book' => 'Lingvistica'
+          ),
+          array(
+            'book' => 'Limbi straine'
+          ),
+          array(
+            'book' => 'Teatru'
+          ),
+          array(
+            'book' => 'Poezie / Literatura'
+          ),
+          array(
+            'book' => 'Atlase, ghiduri turistice'
+          ),
+          array(
+            'book' => 'Istorie'
+          ),
+          array(
             'book' => 'Filozofie'
           ),
           array(
-            'book' => 'Fantasy'
+            'book' => 'Psihologie'
+          ),
+          array(
+            'book' => 'Stiinte sociale, politica'
+          ),
+          array(
+            'book' => 'Marketing si comunicare'
+          ),
+          array(
+            'book' => 'Business si economie'
+          ),
+          array(
+            'book' => 'Drept'
+          ),
+          array(
+            'book' => 'Medicina'
+          ),
+          array(
+            'book' => 'Stiinte exacte'
+          ),
+          array(
+            'book' => 'Natura si mediu'
+          ),
+          array(
+            'book' => 'Tehnica si tehnologie'
+          ),
+          array(
+            'book' => 'Computere si internet'
+          ),
+          array(
+            'book' => 'Dezvoltare personala'
+          ),
+          array(
+            'book' => 'Lifestyle, sport'
           ),
         ));
     }
