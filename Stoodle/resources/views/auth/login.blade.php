7f80f3c5f98ecea8e5c57cfe8e715617f27096f0
GrigoAleex <grigorescu.aleex@gmail.com>
2020-06-26 11:05:07 +0300

    Refactoed login system

--- Stoodle/resources/views/auth/login.blade.php
+++ Stoodle/resources/views/auth/login.blade.php
@@ -3,44 +3,44 @@
 @section('title', 'Logare')
 
 @section('content')
-<div class="container login">
-    <div class="headline">
-        <img src="{{ asset('img/logo.png') }}" alt="Logo">
-        <h1>Autentificare</h1>
-    </div>
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="container login">
+        <div class="headline">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo">
+            <h1>Autentificare</h1>
+        </div>
+        <form method="POST" action="{{ route('login') }}">
+            @csrf
 
-        <div class="form-group">
-            <input id="email" type="email" class="form-control" name="email" placeholder=" " required autocomplete="email" autofocus>
-            <label for="email">{{ __('E-mail') }}</label>
+            <div class="form-group">
+                <input id="email" type="email" class="form-control" name="email" placeholder=" " required autocomplete="email" autofocus>
+                <label for="email">{{ __('E-mail') }}</label>
 
-            @error('email')
+                @error('email')
+                    <small> {{ $message }} </small>
+                @enderror
+            </div>
+
+            <div class="form-group">
+                <input id="password" type="password" class="form-control" name="password" placeholder=" " required autocomplete="current-password">
+                <label for="password">{{ __('Parola') }}</label>
+
+                @error('password')
                 <small> {{ $message }} </small>
-            @enderror
-        </div>
+                @enderror
+            </div>
 
-        <div class="form-group">
-            <input id="password" type="password" class="form-control" name="password" placeholder=" " required autocomplete="current-password">
-            <label for="password">{{ __('Parola') }}</label>
+            <div class="form-group m-0">
+                <a href="{{ route('register') }}">Creeaza-ti un cont!</a><br>
+                <a href="{{ route('password.request') }}">Reseteaza-ti parola!</a>
+            </div>
 
-            @error('password')
-               <small> {{ $message }} </small>
-            @enderror
-        </div>
-
-        <div class="form-group m-0">
-            <a href="{{ route('register') }}">Creeaza-ti un cont!</a><br>
-            <a href="{{ route('password.request') }}">Reseteaza-ti parola!</a>
-        </div>
-
-        <a class="btn btn-outline-dark" 
-            role="button" style="text-transform:none; width: 100%; padding: 1em; margin: .3em 0;">
-            <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" 
-            src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
-            Conecteaza-te cu Google
-        </a>
-        <input type="submit" name="loginsubmit" value="Trimite" class="button" />
-    </form>
-</div>
+            <a class="btn btn-outline-dark" 
+                role="button" style="text-transform:none; width: 100%; padding: 1em; margin: .3em 0;">
+                <img width="20px" style="margin-bottom:3px; margin-right:5px" alt="Google sign-in" 
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/512px-Google_%22G%22_Logo.svg.png" />
+                Conecteaza-te cu Google
+            </a>
+            <input type="submit" name="loginsubmit" value="Trimite" class="button" />
+        </form>
+    </div>
 @endsection
--- Stoodle/resources/views/auth/passwords/email.blade.php
+++ Stoodle/resources/views/auth/passwords/email.blade.php
@@ -1,29 +1,33 @@
 @extends('layouts.app')
 
+@section('title', 'Resetare parola')
+
 @section('content')
-<div class="container login">
-    <div class="headline">
-        <img src="{{ asset('img/logo.png') }}" alt="Logo">
-        <h1>Resetare parola</h1>
-    </div>
-    <!-- @if (session('status'))
-        <div class="alert alert-success" role="alert">
-            {{ session('status') }}
+    <div class="container login">
+        <div class="headline">
+            <img src="{{ asset('img/logo.png') }}" alt="Logo">
+            <h1>Resetare parola</h1>
         </div>
-    @endif -->
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+        @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
+            </div>
+        @endif
 
-        <div class="form-group">
-            <input id="email" type="email" class="form-control" name="email" placeholder=" " required autocomplete="email" autofocus>
-            <label for="email">{{ __('E-mail') }}</label>
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+            <div class="form-group">
+                <input id="email" type="email" class="form-control" name="email" placeholder=" " required autocomplete="email" autofocus>
+                <label for="email">{{ __('E-mail') }}</label>
+                
                 @error('email')
                     <small> {{ $message }} </small>
                 @enderror
-        
-        </div>
-        <input type="submit" name="loginsubmit" value="Trimite link-ul pentru resetare parola" class="button" />
-    </form> 
-</div>
+            
+            </div>
+            <input type="submit" name="loginsubmit" value="Trimite link-ul pentru resetare parola" class="button" />
+        </form> 
+    </div>
 @endsection
