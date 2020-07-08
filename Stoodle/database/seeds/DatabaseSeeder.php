06d8d88b7a869c217e82cbf03c1662f0eb779db1
RobertPlaiasu <robertplaiasu03@gmail.com>
2020-07-08 19:41:03 +0300

    update seeders

--- Stoodle/database/seeds/DatabaseSeeder.php
+++ Stoodle/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
         $this->call(BookSeeder::class);
         $this->call(CountySeeder::class);
         $this->call(PassionSeeder::class);
@@ -19,5 +18,9 @@ class DatabaseSeeder extends Seeder
         $this->call(SubjectSeeder::class);
         $this->call(UniversitySeeder::class);
         $this->call(QuestionSeeder::class);
+        $this->call(PassionTypeSeeder::class);
+        $this->call(ProfilTypeSeeder::class);
+        $this->call(RegionSeeder::class);
+        $this->call(SubjectTypeSeed::class);
     }
 }
