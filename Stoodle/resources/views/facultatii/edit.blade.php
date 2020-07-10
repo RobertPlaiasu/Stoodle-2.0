4f2981c226292f441e6caf93e17c3e9d7c06fa4c
GrigoAleex <grigorescu.aleex@gmail.com>
2020-07-10 13:10:59 +0300

    refactored the facultatii folder

--- Stoodle/resources/views/facultatii/edit.blade.php
+++ Stoodle/resources/views/facultatii/edit.blade.php
@@ -5,10 +5,14 @@
 @section('content')
     <div class="form">
         <div class="container">
+            {{-- Header --}}
             <h1>Editare facultate in baza de date</h1>
             <p>Completeaza formularul de mai jos pentru a edita o faculatate.</p>
+            
             <form action="{{ route('facultati.store') }}" method="POST" id="formular">
                 @csrf
+
+                {{-- Edit college name --}}
                 <div class="form-group">
                     <label for="name">Numele facultatii.</label>
                     <input type="text" id="name" name="name" class="form-control" placeholder="Numele facultatii" value="{{ $college->name }}">
@@ -16,21 +20,28 @@
                         {{ $message }}
                     @enderror
                 </div>
+
+                {{-- Edit the university --}}
                 <div class="form-group">
                     <label for="university">Universitatea de care apartine facultatea.</label>
                     <select class="custom-select" name="university" id="countyPassion">
                         @foreach ( $data['universities'] as $university )
                             <option value="{{ $university->id }}"
+                                {{-- Select the current university --}}
                                 @if ( $university === $college->university)
                                     selected="selected"
                                 @endif    
-                            >{{ $university->name }}</option>
+                            >
+                                {{ $university->name }}
+                            </option>
                         @endforeach
                     </select>
                     @error('university')
                             {{ $message }}
                     @enderror
                 </div>
+
+                {{-- Edit the link to the college's website --}}
                 <div class="form-group">
                     <label for="url">Link catre facultate.</label>
                     <textarea name="url" id="url" rows="1" class="form-control">
@@ -39,6 +50,9 @@
                     @error('url')
                         {{ $message }}
                     @enderror
+                </div>
+
+                {{-- Edit the description of the college --}}
                 <div class="form-group">
                     <label for="description">Descriere facultate.</label>
                     <textarea name="description" id="description" rows="7" class="form-control">
@@ -47,8 +61,9 @@
                     @error('description')
                         {{ $message }}
                     @enderror
-                </div>
-                    
+                </div> 
+                
+                {{-- Edit the addmitence boolean --}}
                 <div class="form-group">
                     <label for="admittance">Necesita facultatea admintere?</label>
                     <select class="custom-select" name="admittance" id="admittanceSelect">
@@ -59,8 +74,12 @@
                             {{ $message }}
                     @enderror
                 </div>
+
+                {{-- Enable editing the form --}}
                 @include('inc.passion')
                 @include('inc.form')
+
+                {{-- Submit the form --}}
                 <input type="submit" value="Trimite Formular" name="formularsubmit" class="button">
             </form>
         </div>
--- Stoodle/resources/views/facultatii/index.blade.php
+++ Stoodle/resources/views/facultatii/index.blade.php
@@ -3,35 +3,37 @@
 @section('title', 'Acasa')
 
 @section('content')
-    <div class="w-100 text-center">
+    <div class="text-center">
+        {{-- Search Bar --}}
         <div id="search" class="mx-4 my-3">
             <input onkeyup="sort()" class="form-control"
                 type="text" placeholder="cauta" id="search_field" aria-label="Search">
         </div>
         
+        {{-- Display all the colleges that exist --}}
         @if (count($colleges) > 0)
           @foreach($colleges as $college)
-
               <div class="card">
-                    {{-- * Image --}}
+                    {{-- Image --}}
                     <div class="backgrounded"></div>
 
-                    {{-- * Name --}}
+                    {{-- Name --}}
                     <div class="name">
                         {{ $college->name }}                 
                     </div>
 
-                    {{--* University && Compability && County --}}
+                    {{-- Features --}}
                     <div class="prop text-center">
                         <div class="col">
 
-                            {{-- * University --}}
+                            {{-- University --}}
                             <div class="row">
                                 <div class="col">
                                     {{ $college->university->name }}
                                 </div>
                             </div>
 
+                            {{-- Compability with the student --}}
                             <div class="row justify-content-between">
                                 <div class="col">
                                     {{-- {{ $college->compability }} --}}
@@ -42,6 +44,8 @@
                                     <i class="fas fa-city"></i>
                                 </div>
                             </div>
+
+                            {{-- College's profile --}}
                             <div class="row">
                                 <div class="col">
                                     {{ $college->profil->name }}
@@ -50,42 +54,24 @@
                             </div>
                         </div>
                     </div>
+
+                    {{-- Add the colleges to favorites --}}
                     <div class="row-lg-3 fav text-center">
                         Adaugare la favorite
                         {{-- <form action="./favoriteAlg.php" method="post">
                             {{ $college->favoriteCollegeFound() }}
                         </form> --}}
                     </div>
+
+                    {{-- Know more about the college --}}
                     <div class="row-lg-3 extra text-center">
                         <a href="facultati/{{ $college->id }}">Afla mai mult</a>
                     </div>
                 </div>
             @endforeach
         @else 
-        
+            {{-- If there are no colleges display next content --}}
             <h1>Nu am reusit sa incercam facultatile din baza de date</h1>
-            
         @endif
-        
-        
-        <script>
-            function sort() {
-                // Declare variables
-                var input = document.getElementById('search_field');
-                var filter = input.value.toUpperCase();
-                var faculties = document.getElementsByClassName("name");
-                var cards = document.getElementsByClassName("card");
-        
-                // Loop through all list items, and hide those who don't match the search query
-                for (i = 0; i < faculties.length; i++) {
-                    txtValue = facultati[i].textContent || faculties[i].innerText;
-                    if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                        cards[i].style.display = "";
-                    } else {
-                        cards[i].style.display = "none";
-                    }
-                }
-            }
-        </script>
     </div>
 @endsection
