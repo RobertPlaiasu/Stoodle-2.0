938119eb4665be9dbe0207b1e9264c678ff13f05
GrigoAleex <grigorescu.aleex@gmail.com>
2020-07-23 15:25:03 +0300

    Created tests for the auth

---
+++ Stoodle/tests/Feature/Auth/LoginTest.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response
+            ->assertSuccessful()
+            ->assertViewIs('auth.login');
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/facultati');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/facultati');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+        
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+        
+        $response
+            ->assertRedirect('/login')
+            ->assertSessionHasErrors('email');
+
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_remember_me_functionality()
+    {
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+        
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+        
+        $response->assertRedirect('/facultati');
+        // cookie assertion goes here
+        $this->assertAuthenticatedAs($user);
+    }
+}
---
+++ Stoodle/tests/Feature/Auth/RegisterTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class RegisterTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_view_a_register_form()
+    {
+        $response = $this->get('/register');
+
+        $response
+            ->assertSuccessful()
+            ->assertViewIs('auth.register');
+    }
+
+    public function test_user_cannot_view_a_register_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get('/register');
+
+        $response->assertRedirect('/facultati');
+    }
+
+    public function a_user_can_register_with_correct_credentials()
+    {
+        $response = $this->post('/register', [
+            'name' => 'Joe',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'passwordtest'
+        ]);
+
+        $response
+            ->assertSuccessful()
+            ->assertRedirect('/facultati');
+    }
+
+    public function test_user_cannot_register_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+        
+        $response = $this->from('/register')->post('/login',[
+            'name' => 'Joe',
+            'email' => 'testemail@test.com',
+            'password' => 'passwordtest',
+            'password_confirmation' => 'invalid-confirmation'
+        ]);
+        
+        $response
+            ->assertRedirect('/register')
+            ->assertSessionHasErrors('email');
+
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+}
