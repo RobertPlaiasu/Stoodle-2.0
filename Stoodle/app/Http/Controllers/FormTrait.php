3f6ec3ed46c2ba8fd6e3a05df35fd07af8d9e6bb
GrigoAleex <grigorescu.aleex@gmail.com>
2020-07-23 15:06:59 +0300

    Meeting

--- Stoodle/app/Http/Controllers/FormTrait.php
+++ Stoodle/app/Http/Controllers/FormTrait.php
@@ -6,6 +6,7 @@ trait FormTrait
 {
     private function verifyMultipleInputs(int $input1 , int $input2, int $input3)
     {
+        //tODO: 3= TEST
         return ($input1 == $input2 || $input1 == $input3 || $input2 == $input3);
     }
 
